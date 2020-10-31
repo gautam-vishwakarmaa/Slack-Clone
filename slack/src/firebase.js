3a4c636bace6166457c81bf3f6def3ea4dc047eb
Mr. Vishwakarma <63588615+gautam-vishwakarmaa@users.noreply.github.com>
2020-10-31 08:07:00 -0700

    Update firebase.js

--- slack/src/firebase.js
+++ slack/src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATrxMP_OpbFbkUTdWj2RgJRUJ6t-C0mmE",
-    authDomain: "slack-40ef2.firebaseapp.com",
-    databaseURL: "https://slack-40ef2.firebaseio.com",
-    projectId: "slack-40ef2",
-    storageBucket: "slack-40ef2.appspot.com",
-    messagingSenderId: "628343792234",
-    appId: "1:628343792234:web:4a55e5968d03e38c8fe013",
-    measurementId: "G-NMV9LJ66RV"
+//     your api key
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
